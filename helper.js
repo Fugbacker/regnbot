946aeef36b4c4ffc82b3eb0c2f394cb47e8358e5
Fugbacker <evgenzolotoff@gmail.com>
2024-01-11 17:08:00 +0400

    fix

--- helper.js
+++ helper.js
@@ -77,43 +77,8 @@ module.exports = {
         return result.join('\n')
       },
       dadataTokenChanger: async function dadataTokenChanger () {
-        let dadataToken = 'Token 431c3958f002f6f546afe128257059d372093aa2'
-        const tokenList = [
-          'Token 431c3958f002f6f546afe128257059d372093aa2',
-          'Token 1a86eedfc8da905b34669e441476d13d8ccc4691',
-          'Token 0d5ab8f4aabc1cc02c29b2d759e0ebde7254a4b7',
-          'Token 3ed91c052b049be7c81567f637a421153fd2a893',
-          'Token 70b8dda637580dd14625d9296f24945f2a6fc4f9',
-          'Token cc6c5060a102fea6d7e9fca62b723140b71fe26d',
-          'Token b34e052b0d7e9ee8ee4bed6e9b6c37f65c6bf19d',
-          'Token d96100ae95f29bf1e836953ab1d8806f699b32bd'
-         ]
-
-         const now = new Date();
-         const day = now.getDate();
-         try {
-          const response = await axios("https://dadata.ru/api/v2/findById", {
-            headers: {
-              Authorization: `Bearer ${dadataToken}`,
-            },
-          });
-          // if (response.data.response.status === 403) {
-          //   // Токен исчерпал лимит запросов
-          //   const tokenIndex = tokenList.indexOf(token)
-          //   dadataToken = tokenList[tokenIndex+1];
-          // }
-         } catch {
-          const tokenIndex = tokenList.indexOf(dadataToken)
-          dadataToken = tokenList[tokenIndex+1];
-         }
-
-
-
-        // Если наступили новые сутки, начинаем работу с первого в списке токена
-        if (day !== now.getDate()) {
-          dadataToken = 'Token 431c3958f002f6f546afe128257059d372093aa2'
-          console.log('refresh token')
-        }
-       return dadataToken
+       const {data} = await axios('https://goskadastr.su/api/token')
+       console.log("TOKEN", data)
+       return data
       }
 }
